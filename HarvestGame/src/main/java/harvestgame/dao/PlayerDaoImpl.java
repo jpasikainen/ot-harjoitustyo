e759047aeca90dd9e4d1e87331b24f07d0be78c2
jpasikainen <jpasikainen@pm.me>
2020-12-07 11:51:42 +0200

    Added docs

--- HarvestGame/src/main/java/harvestgame/dao/PlayerDaoImpl.java
+++ HarvestGame/src/main/java/harvestgame/dao/PlayerDaoImpl.java
@@ -1,8 +1,23 @@
 package harvestgame.dao;
 
 public interface PlayerDaoImpl {
+    /**
+     * Write balance on the database
+     */
     void writeBalance();
+
+    /**
+     * @return money
+     */
     int getBalance();
+
+    /**
+     * @param amount negative or positive value that gets added or reduced from the balance
+     */
     void changeBalance(int amount);
+
+    /**
+     * Reset money on the database to the default value
+     */
     void resetData();
 }
--- HarvestGame/src/main/java/harvestgame/dao/StoreDaoImpl.java
+++ HarvestGame/src/main/java/harvestgame/dao/StoreDaoImpl.java
@@ -5,7 +5,21 @@ import harvestgame.core.Plant;
 import java.util.ArrayList;
 
 public interface StoreDaoImpl {
+    /**
+     * @return list of plants
+     */
     ArrayList<Plant> getPlants();
+
+    /**
+     * @param plantId id of the plant being bought
+     * @param player location of the wallet
+     * @return clone of the plant from database with the same id
+     */
     Plant buyPlant(int plantId, PlayerDao player);
+
+    /**
+     * @param plantId id of the plant being bought
+     * @return plant from the database, not a clone
+     */
     Plant getPlant(int plantId);
 }
